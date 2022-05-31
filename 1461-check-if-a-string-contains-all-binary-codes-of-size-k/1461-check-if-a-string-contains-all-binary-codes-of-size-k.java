b3823310e16e34601fe1bdd9491008607c470263
Abhishek Mallick <106394426+Abhishek-Mallick@users.noreply.github.com>
2022-05-31 12:25:45 +0530

    Time: 16 ms (92.71%), Space: 62.3 MB (60.94%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,14 +1,17 @@
 class Solution {
     public boolean hasAllCodes(String s, int k) {
-         int need = 1 << k;
-        Set<String> got = new HashSet<String>();
+        int need = 1 << k;
+        boolean[] got = new boolean[need];
+        int allOne = need - 1;
+        int hashVal = 0;
 
-        for (int i = k; i <= s.length(); i++) {
-            String a = s.substring(i - k, i);
-            if (!got.contains(a)) {
-                got.add(a);
+        for (int i = 0; i < s.length(); i++) {
+            // calculate hash for s.substr(i-k+1,i+1)
+            hashVal = ((hashVal << 1) & allOne) | (s.charAt(i) - '0');
+            // hash only available when i-k+1 > 0
+            if (i >= k - 1 && !got[hashVal]) {
+                got[hashVal] = true;
                 need--;
-                // return true when found all occurrences
                 if (need == 0) {
                     return true;
                 }
