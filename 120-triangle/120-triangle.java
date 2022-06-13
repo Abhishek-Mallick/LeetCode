f428955681eb879d689c6eb5c108c1727b550793
Abhishek Mallick <106394426+Abhishek-Mallick@users.noreply.github.com>
2022-06-13 18:44:14 +0530

    Time: 5 ms (49.59%), Space: 42.2 MB (80.70%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,21 +1,13 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int dp[][] = new int[triangle.size()][triangle.get(triangle.size()-1).size()];
-        for(int el[] : dp)
-            Arrays.fill(el,Integer.MAX_VALUE);
-        return sumfind(triangle,0,0,dp);
-    }
-    private int sumfind(List<List<Integer>> triangle,int row,int col,int dp[][])
-    {
-        if(row == triangle.size())
-            return 0;
-        
-        if(dp[row][col] != Integer.MAX_VALUE)
-            return dp[row][col];
-        
-        int min = Math.min(sumfind(triangle,row+1,col,dp),sumfind(triangle,row+1,col+1,dp));
-        dp[row][col] = triangle.get(row).get(col)+min;
-        
-        return dp[row][col];
+        int dp[] = new int[triangle.size()+1];
+        for(int i=triangle.size()-1;i>=0;i--)
+        {
+            for(int j=0;j<triangle.get(i).size();j++)
+            {
+                dp[j] = Math.min(dp[j],dp[j+1])+triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
     }
 }
