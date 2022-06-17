bed0358780019661825c1d44a11950741a18cabc
Abhishek Mallick <106394426+Abhishek-Mallick@users.noreply.github.com>
2022-06-17 21:29:14 +0530

    Time: 1 ms (76.65%), Space: 44 MB (18.28%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -14,37 +14,25 @@
  * }
  */
 class Solution {
-    
-    private int CameraCount = 0;
-    private int needCamera = 0;
-    private int hasCamera = 1;
-    private int coveredNode = 2;
-    private int notneeded = 3;
-    
     public int minCameraCover(TreeNode root) {
-        CameraCount = CameraPlacement(root) == needCamera?CameraCount+1:CameraCount;
-        return CameraCount;
+        int ans[] = solve(root);
+        return Math.min(ans[1],ans[2]);
     }
     
-    private int CameraPlacement(TreeNode root)
+    public int[] solve(TreeNode node)
     {
-        if(root == null)
-            return notneeded;
-        if(root.left == null && root.right == null)
-            return needCamera;
+        if(node == null)
+            return new int[]{0,0,9999};
+        int l[] = solve(node.left);
+        int r[] = solve(node.right);
         
-        int left = CameraPlacement(root.left);
-        int right = CameraPlacement(root.right);
+        int ml = Math.min(l[1],l[2]);
+        int mr = Math.min(r[1],r[2]);
         
-        if(left == needCamera || right == needCamera)
-        {
-            CameraCount++;
-            return hasCamera;
-        } 
+        int d0 = l[1]+r[1];
+        int d1 = Math.min(l[2]+mr , r[2]+ml);
+        int d2 = 1+Math.min(l[0],ml) + Math.min(r[0],mr);
         
-        if(left == hasCamera || right == hasCamera)
-            return coveredNode;
-        
-        return needCamera;
+        return new int[]{d0,d1,d2};
     }
 }
