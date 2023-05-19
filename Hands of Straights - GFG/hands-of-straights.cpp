12d44467d5eac7a8694d52248dffc97b1a096601
Abhishek Mallick <106394426+Abhishek-Mallick@users.noreply.github.com>
2023-05-20 04:18:02 +0530

    Added solution - LeetHub

---
+++ Hands of Straights - GFG/hands-of-straights.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool isStraightHand(int N, int groupSize, vector<int> &hand) {
+        map<int, int> mp;
+        for (int i : hand) mp[i]++;
+        for (auto it : mp) {
+            if (mp[it.first] > 0) {
+                for (int i = groupSize - 1; i >= 0; --i) {
+                    if ((mp[it.first + i] -= mp[it.first]) < 0) return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, groupSize;
+        cin >> N >> groupSize;
+
+        vector<int> hand(N);
+        for (int i = 0; i < N; i++) cin >> hand[i];
+
+        Solution obj;
+        int ans = obj.isStraightHand(N, groupSize, hand);
+        if (ans)
+            cout << "True" << endl;
+        else
+            cout << "False" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
