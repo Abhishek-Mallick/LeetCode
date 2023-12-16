39f0c8221663d42f202c383c220fb6bd2303259e
Abhishek Mallick <106394426+Abhishek-Mallick@users.noreply.github.com>
2023-12-16 17:37:00 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/String's Count/strings-count.cpp
@@ -0,0 +1,22 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+long long int countStr(long long int n);
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long int n;
+        cin >> n;
+        cout << countStr(n) << endl;
+    }
+return 0;
+}
+// } Driver Code Ends
+
+
+long long int countStr(long long int n){
+    long long ans = 1+(n*2)+(n*((n*n)-1)/2);
+    return ans; 
+}
