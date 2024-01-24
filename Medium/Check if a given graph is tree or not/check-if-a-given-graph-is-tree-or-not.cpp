ddc27f5168c6846b7c319cc5e6854268df737a70
Abhishek Mallick <106394426+Abhishek-Mallick@users.noreply.github.com>
2024-01-24 23:50:15 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Check if a given graph is tree or not/check-if-a-given-graph-is-tree-or-not.cpp
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Matrix
+{
+public:
+    template <class T>
+    static void input(vector<vector<T>> &A,int n,int m)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                scanf("%d ",&A[i][j]);
+            }
+        }
+    }
+ 
+    template <class T>
+    static void print(vector<vector<T>> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            for (int j = 0; j < A[i].size(); j++)
+            {
+                cout << A[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int isTree(int n, int m, vector<vector<int>> &edges) {
+        if(m!=n-1)
+            return false;
+            
+        vector<bool>vis(n,false);
+        vector<int>adj[n];
+        
+        for(int i=0;i<edges.size();i++)
+        {
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+        }
+        
+        int node=0;
+        queue<int>q;
+    
+        vis[node]=true;
+        int visited=1;
+        q.push(node);
+        
+        while(q.size())
+        {
+            int curr=q.front();
+            q.pop();
+            for(auto it:adj[curr])
+            {
+                if(!vis[it])
+                {
+                    vis[it]=true;
+                    visited++;
+                    q.push(it);
+                }
+            }
+        }
+        return (visited==n);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n; 
+        scanf("%d",&n);
+        
+        
+        int m; 
+        scanf("%d",&m);
+        
+        
+        vector<vector<int>> edges(m, vector<int>(2));
+        Matrix::input(edges,m,2);
+        
+        Solution obj;
+        int res = obj.isTree(n, m, edges);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
