c4629b1c689cc9e6913d5c1fa58a04c171b55f50
Abhishek Mallick <106394426+Abhishek-Mallick@users.noreply.github.com>
2024-07-16 01:53:44 +0530

    Time: 62 ms (80.41%) | Memory: 21.7 MB (70.40%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -28,6 +28,23 @@ public:
         reverse(s.begin(),s.end());
         return totalPoints;
     }
+    int scoreStrings2(string& s, string resString, int point)
+    {
+        int totalPoints = 0, n = s.size();
+        int writeIndex = 0;
+
+        for(int readIndex=0;readIndex<n;readIndex++)
+        {
+            s[writeIndex++] = s[readIndex];
+            if(writeIndex > 1 && s[writeIndex - 1] == resString[1] && s[writeIndex - 2] == resString[0])
+            {
+                writeIndex -= 2;
+                totalPoints += point;
+            }
+        }
+        s = s.substr(0,writeIndex);
+        return totalPoints;
+    }
     int maximumGain(string s, int x, int y) {
         /*
         cdbcbbaaabab
@@ -46,6 +63,6 @@ public:
         string s2 = "ba";
         if(x < y)
             swap(x,y), swap(s1,s2);
-        return scoreStrings(s,s1,x) + scoreStrings(s,s2,y);
+        return scoreStrings2(s,s1,x) + scoreStrings2(s,s2,y);
     }
 };
