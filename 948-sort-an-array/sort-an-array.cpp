094eac0c5b5dc26b37be57ab487c8a69d515967e
Abhishek Mallick <106394426+Abhishek-Mallick@users.noreply.github.com>
2024-07-26 04:25:34 +0530

    Time: 323 ms (39.30%) | Memory: 176.1 MB (32.29%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    void merge(vector<int>& array, int const left, int const mid,
+            int const right)
+    {
+        int const subArrayOne = mid - left + 1;
+        int const subArrayTwo = right - mid;
+
+        // Create temp arrays
+        auto *leftArray = new int[subArrayOne],
+            *rightArray = new int[subArrayTwo];
+
+        // Copy data to temp arrays leftArray[] and rightArray[]
+        for (auto i = 0; i < subArrayOne; i++)
+            leftArray[i] = array[left + i];
+        for (auto j = 0; j < subArrayTwo; j++)
+            rightArray[j] = array[mid + 1 + j];
+
+        auto indexOfSubArrayOne = 0, indexOfSubArrayTwo = 0;
+        int indexOfMergedArray = left;
+
+        // Merge the temp arrays back into array[left..right]
+        while (indexOfSubArrayOne < subArrayOne
+            && indexOfSubArrayTwo < subArrayTwo) {
+            if (leftArray[indexOfSubArrayOne]
+                <= rightArray[indexOfSubArrayTwo]) {
+                array[indexOfMergedArray]
+                    = leftArray[indexOfSubArrayOne];
+                indexOfSubArrayOne++;
+            }
+            else {
+                array[indexOfMergedArray]
+                    = rightArray[indexOfSubArrayTwo];
+                indexOfSubArrayTwo++;
+            }
+            indexOfMergedArray++;
+        }
+
+        // Copy the remaining elements of
+        // left[], if there are any
+        while (indexOfSubArrayOne < subArrayOne) {
+            array[indexOfMergedArray]
+                = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+            indexOfMergedArray++;
+        }
+
+        // Copy the remaining elements of
+        // right[], if there are any
+        while (indexOfSubArrayTwo < subArrayTwo) {
+            array[indexOfMergedArray]
+                = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+            indexOfMergedArray++;
+        }
+        delete[] leftArray;
+        delete[] rightArray;
+    }
+
+    // begin is for left index and end is right index
+    // of the sub-array of arr to be sorted
+    void mergeSort(vector<int>& array, int const begin, int const end)
+    {
+        if (begin >= end)
+            return;
+
+        int mid = begin + (end - begin) / 2;
+        mergeSort(array, begin, mid);
+        mergeSort(array, mid + 1, end);
+        merge(array, begin, mid, end);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+};
