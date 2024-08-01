f5e3d64a64fd1331319bf9ea0fb02bc5a3e45727
Abhishek Mallick <106394426+Abhishek-Mallick@users.noreply.github.com>
2024-08-02 03:48:15 +0530

    Time: 4 ms (82.84%) | Memory: 17.6 MB (28.98%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -3,7 +3,12 @@ public:
     int countSeniors(vector<string>& details) {
         int res = 0;
         for(string it : details) {
-            int temp = stoi(it.substr(11,2));
+            // int temp = stoi(it.substr(11,2));
+
+            int digit1 = it[11] - '0';
+            int digit2 = it[12] - '0';
+            int temp = digit1*10 + digit2;
+
             if(temp > 60)
                 res++;
         }
