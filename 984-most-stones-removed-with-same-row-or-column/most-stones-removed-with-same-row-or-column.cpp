bf9b0d81a3fd76f3b79637d0655cf665c496d49c
Abhishek Mallick <106394426+Abhishek-Mallick@users.noreply.github.com>
2024-08-30 04:22:15 +0530

    Time: 62 ms (34.33%) | Memory: 18.3 MB (79.16%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> parent;
+    vector<int> rank;
+    int n;
+    
+    int find(int i) {
+        if(parent[i] != i)
+            parent[i] = find(parent[i]);
+        
+        return parent[i];
+    }
+    
+    void Union(int i, int j) {
+        int root_i = find(i);
+        int root_j = find(j);
+        
+        if(root_i != root_j) {
+            if(rank[root_i] > rank[root_j]) {
+                parent[root_j] = root_i;
+            } else  if(rank[root_i] < rank[root_j]) {
+                parent[root_i] = root_j;
+            } else {
+                parent[root_j] = root_i;
+            }
+        }
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        n = stones.size();
+        parent.resize(n);
+        rank.resize(n);
+        
+        for(int i = 0; i<n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+        
+        for(int i = 0; i<n; i++) {
+            for(int j = i+1; j<n; j++) {
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1])
+                    Union(i, j);
+            }
+        }
+        
+        int groups = 0;
+        for(int i = 0; i<n; i++) {
+            if(parent[i] == i) groups++;
+        }
+        
+        return n-groups;
+    }
+};
