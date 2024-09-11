56d2c32ecb99bf95e55280748429180f541a72af
Abhishek Mallick <106394426+Abhishek-Mallick@users.noreply.github.com>
2024-09-12 05:07:02 +0530

    Time: 4 ms (41.37%) | Memory: 7.6 MB (81.20%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int count = 0;
+        while (start > 0 || goal > 0) {
+            if ((start & 1) != (goal & 1)) {
+                count++;
+            }
+            start >>= 1;
+            goal >>= 1;
+        }
+        return count;
+    }
+};
